e15db0717846874608a61edd8d991e3bbb4bb16a
Philip Ekalale <philipekalale07@gmail.com>
2025-11-23 14:17:03 +0300

    added controllers for income and expense

---
+++ backend/controllers/IncomeControllers.js
@@ -0,0 +1,33 @@
+import Income from "../models/Income";
+
+//adding income
+export const addIncome = async(req,res)=>
+{
+    try{
+        const income =new Income(req.body);
+        await Income.save();
+        res.status(201).json(income);
+    }
+    catch(err){
+        res.status(400).json({error:error.message});
+    }
+};
+
+//get all income
+export const getIncome =async(req,res)=>{
+    try{
+        const income =await Income.find().sort({date: -1});
+        res.json(income);
+    }catch(err){
+       res.status(500).json({error:error.mesage});
+    }
+};
+//delete income
+export const deleteIncome = async(req,res)=>{
+    try{
+        await Income.findByIdAndDelete(req.params.id);
+        res.json({message:"income deleted"});
+    }catch(err){
+        res.status(500).json({error:error.message});
+    }
+};
